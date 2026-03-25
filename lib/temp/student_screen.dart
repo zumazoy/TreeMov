dcac1f218aac897b6582feeab5d333f2ada8da3e
Lev <kurganovl07@mail.ru>
2026-03-25 09:47:58 +0300

    Обновлённый student_screen

--- lib/temp/student_screen.dart
+++ lib/temp/student_screen.dart
@@ -16,6 +16,12 @@ class StudentScreen extends StatefulWidget {
 class _StudentScreenState extends State<StudentScreen> {
   late int _currentIndex;
 
+  final List<Widget> _pages = const [
+    CalendarKidScreen(),
+    RatingScreen(),
+    StudentProfileScreen(),
+  ];
+
   @override
   void initState() {
     super.initState();
@@ -25,14 +31,7 @@ class _StudentScreenState extends State<StudentScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: IndexedStack(
-        index: _currentIndex,
-        children: const [
-          CalendarKidScreen(),
-          RatingScreen(),
-          StudentProfileScreen(),
-        ],
-      ),
+      body: _pages[_currentIndex],
       bottomNavigationBar: ChildBottomNavigationBar(
         currentIndex: _currentIndex,
         onTap: (index) {
